385b632153fc129c1fd061af7600abee4bb75f17
lijinwu <1025462978@qq.com>
2021-02-18 10:28:57 +0800

    mvc代码的启动过程

--- src/main/java/com/ljw/spring/mvc/s2/mvc/AppConfig.java
+++ src/main/java/com/ljw/spring/mvc/s2/mvc/AppConfig.java
@@ -10,11 +10,21 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
  * 这里装配之后，会影响普通的@Controller映射，
  * 不共存
  *
- * 如果这里要启用，放开注解
+ * 如果这里要启用，放开@Configuration注解，生成bean
+ *
+ * 注解@EnableWebMvc，用来替换xml配置的注解加载
+ * 这里是把需要的HandlerMapping加载进来
+ * 包含xml，jsp等一些
+ * 
+ * 注解@EnableWebMvc加载
+ * RequestMappingHandlerMapping
+ * HandleResolver
+ * HandleAdapter
+ * ViewResolver
  *
  */
 //@Configuration
-//@EnableWebMvc
+@EnableWebMvc
 public class AppConfig implements WebMvcConfigurer {
 
     @Override
@@ -36,6 +46,11 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
+        /**
+         * 装配拦截器-5
+         *
+         * 调用钩子方法。
+         * 最终调用到自定义的实现了WebMvcConfigurer的类，调用对应实现的钩子方法的实现方法
+         */
     }
 }
