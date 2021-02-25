bdd4e1cc99e0e4523e08efe9b8095d90f0014302
lijinwu <1025462978@qq.com>
2021-02-25 17:22:38 +0800

    创建

--- src/main/java/com/ljw/spring/mvc/s2/controller/MyTestController.java
+++ src/main/java/com/ljw/spring/mvc/s2/controller/MyTestController.java
@@ -1,6 +1,8 @@
 package com.ljw.spring.mvc.s2.controller;
 
+import com.ljw.spring.mvc.s2.vo.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +16,21 @@ public class MyTestController {
 
     @RequestMapping("/test")
     @ResponseBody
-    public Map getTest(){
+    public Map getTest(@RequestBody User user){
         System.out.println("==========MyTestController.getTest" );
 
         Map map = new HashMap();
         map.put("code", 200);
         map.put("result", "test");
+        map.put("user", user);
+
         return map;
     }
 
+    /**
+     * 这里使用了其他映射
+     * @return
+     */
     @RequestMapping("/ok")
     public String getOk(){
         System.out.println("==========MyTestController.getOK" );
---
+++ src/main/java/com/ljw/spring/mvc/s2/vo/pojo/User.java
@@ -0,0 +1,43 @@
+package com.ljw.spring.mvc.s2.vo.pojo;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private String name;
+    private Integer age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
